f960321c863a5ad5fd1bed0f85ff0d756c388cae
Supersagee <williamdsage@gmail.com>
2022-01-31 22:30:36 -0600

    first commit

---
+++ ClassesExercise1Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise1Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var caddy = new Car();
+            caddy.Make = "Cadillac";
+            caddy.Model = "CTS-V";
+            caddy.Year = 2012;
+
+            var merc = new Car("Mercedes", "AMG GT3 EVO", 2020);
+
+            var cars = new List<Car>();
+            cars.Add(caddy);
+            cars.Add(merc);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Make} --- {car.Model} --- {car.Year}");
+            }
+        }
+    }
+}
